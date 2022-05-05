18ef60c63eda8bd87df75e2097bde6d5f0221cb5
Priyanka <priyankahansi95@gmail.com>
2022-05-05 15:02:46 +0530

    added:delete contact

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,46 +1,59 @@
 package com.bridgelabz;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-import java.io.FileReader;
-import java.io.FileWriter;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 
-public class AddressBook {
+class AddressBook
+{
+    // class variable
     static final Scanner scanner = new Scanner(System.in);
     static Set<String> emptyContacts = new HashSet<>();
     static Set<String> nonEmptyContacts = new HashSet<>();
 
     // instance method
-    void createNewContact() throws Exception {
+    void createNewContact() throws Exception
+    {
         System.out.print("Enter name of the contact:");
         String contactName = scanner.nextLine();
         File file = new File(contactName);
-        if (file.exists()) {
+        if (file.exists())
+        {
             System.out.println("contact " + file.getName() + " already exists!");
-        } else {
-            if (file.createNewFile()) {
+        }
+        else
+        {
+            if (file.createNewFile())
+            {
                 System.out.println("new contact " + file.getName() + " is created successfully");
                 emptyContacts.add(file.getName());
-            } else {
+            }
+            else
+            {
                 System.out.println("file creation failed!");
             }
         }
 
     }
-
-    void writeFile(String fileName, String content) throws Exception {
+    void writeFile(String fileName,String content)throws Exception
+    {
         FileWriter fw = new FileWriter(fileName);
         fw.write(content);
         fw.close();
+        nonEmptyContacts.add(fileName);
+        System.out.println("the given contents are successfully added in " + fileName);
     }
-
-    void fillContactDetails() throws Exception {
+    void fillContactDetails()throws Exception
+    {
         System.out.print("enter empty contact name which is going to be filed:");
         String contactName = scanner.nextLine();
-        if (emptyContacts.contains(contactName)) {
+        if (emptyContacts.contains(contactName))
+        {
             String details = "";
             System.out.print("enter first name:");
             details += scanner.nextLine() + "\n";
@@ -58,90 +71,112 @@ public class AddressBook {
             details += scanner.nextLine() + "\n";
             writeFile(contactName, details);
             emptyContacts.remove(contactName);
-            nonEmptyContacts.add(contactName);
-        } else {
+        }
+        else
+        {
             System.out.println(contactName + " is not empty contact or it is not created");
             System.out.println("use other option 1 to create new contact or option 5 to edit already created one");
         }
 
     }
 
-    void displayAllContacts() {
-
+    void displayAllContacts()
+    {
         boolean flag = false;
-        if (emptyContacts.size() != 0) {
+        if (emptyContacts.size() != 0)
+        {
             System.out.println("the empty contacts are:");
-            for (String contact : emptyContacts) {
+            for (String contact : emptyContacts)
+            {
                 System.out.println(contact);
             }
-            flag = true;
+            flag=true;
         }
-        if (nonEmptyContacts.size() != 0) {
+        if (nonEmptyContacts.size() != 0)
+        {
             System.out.println("the non empty contacts are:");
-            for (String contact : nonEmptyContacts) {
+            for (String contact : nonEmptyContacts)
+            {
                 System.out.println(contact);
             }
-            flag = true;
+            flag=true;
         }
-        if (!flag) {
+        if(!flag)
+        {
             System.out.println("no contacts are created yet");
         }
 
     }
-
-    void readFile(String fileName) throws Exception {
+    void readFile(String fileName)throws Exception
+    {
         FileReader fileReader = new FileReader(fileName);
         int character;
-        while ((character = fileReader.read()) != -1) {
+        while ((character = fileReader.read()) != -1)
+        {
             System.out.print((char) character);
         }
         fileReader.close();
     }
-
-    void viewContactInfo() throws Exception {
+    void viewContactInfo()throws Exception
+    {
         System.out.print("enter name of the contact to view:");
         String contactName = scanner.nextLine();
-        if (emptyContacts.contains(contactName)) {
-            System.out.println("pleast fill the contact " + contactName + " before viewing it");
+        if(emptyContacts.contains(contactName))
+        {
+            System.out.println("pleast fill the contact "+ contactName+ " before viewing it");
             return;
-        } else if (!nonEmptyContacts.contains(contactName)) {
-            System.out.println("please create the contact " + contactName + " before viewing it");
+        }
+        else if(!nonEmptyContacts.contains(contactName))
+        {
+            System.out.println("please create the contact "+contactName+" before viewing it");
             return;
         }
         System.out.println("the content of " + contactName + " is:");
         readFile(contactName);
     }
-
-    void editContactInfo() throws Exception {
+    void editContactInfo()throws Exception
+    {
         System.out.print("enter name of the contact to edit:");
         String contactName = scanner.nextLine();
-        if (emptyContacts.contains(contactName)) {
-            System.out.println("pleast fill the contact " + contactName + " before editing it");
+        if(emptyContacts.contains(contactName))
+        {
+            System.out.println("pleast fill the contact "+ contactName + " before editing it");
             return;
-        } else if (!nonEmptyContacts.contains(contactName)) {
-            System.out.println("please create the contact " + contactName + " before editing it");
+        }
+        else if(!nonEmptyContacts.contains(contactName))
+        {
+            System.out.println("please create the contact "+ contactName +" before editing it");
             return;
         }
         System.out.println("The content of " + contactName + " at present is:");
         readFile(contactName);
         System.out.println("Enter the 7 lines of  new content to write:");
-        String newContent = "";
-        for (int i = 1; i <= 7; i++) {
+        String newContent="";
+        for (int i = 1; i <= 7; i++)
+        {
             newContent += scanner.nextLine() + "\n";
         }
         String option;
-        do {
-            System.out.println("enter... S for SAVE        C for CANCEL");
-            option = scanner.nextLine();
-        } while (!(option.equalsIgnoreCase("s") || option.equalsIgnoreCase("c")));
-        switch (option) {
+        do
+        {
+            System.out.println("enter... S for SAVE     SA for SAVE AS      C for CANCEL");
+            option = scanner.nextLine().trim().toLowerCase();
+        } while (!(option.equals("s")  || option.equals("sa")) || option.equals("c"));
+        switch(option)
+        {
+            //the new contents are saved as givenfilename
             case "s":
-            case "S":
                 writeFile(contactName, newContent);
                 System.out.println(contactName + " is saved with new content");
                 break;
+            //the new contents are saved as givenfilenamewithoutextension.csv
+            case "sa":
+                String extension = ".csv";
+                String newContactName = contactName.replaceFirst("[.][^.]+$", "") + extension;
+                writeFile(newContactName, newContent);
+                nonEmptyContacts.add(newContactName);
+                break;
             case "c":
-            case "C":
                 System.out.println("changes are not saved");
                 return;
             default:
@@ -149,20 +184,54 @@ public class AddressBook {
         }
 
     }
+    void deleteContact()
+    {
+        System.out.print("enter contact name to delete:");
+        String contactName = scanner.nextLine().trim();
+        File file = new File(contactName);
+        if(file.exists())
+        {
+            if(file.delete())
+            {
+                if(emptyContacts.contains(contactName))
+                {
+                    emptyContacts.remove(contactName);
+                }
+                else
+                {
+                    nonEmptyContacts.remove(contactName);
+                }
+                System.out.println(contactName + " is deleted succesfully");
+            }
+            else
+            {
+                System.out.println("file deletion failed");
+            }
 
-    public static void main(String[] args) throws Exception {
+        }
+        else
+        {
+            System.out.println(contactName + "doesn't exists");
+        }
+    }
+
+    public static void main(String[] args) throws Exception
+    {
         AddressBook addressBook = new AddressBook();
-        while (true) {
+        while (true)
+        {
             System.out.println("-----------------------------");
             System.out.println("1.create a new contact");
             System.out.println("2.fill contact details");
             System.out.println("3.display all contacts");
             System.out.println("4.view contact information");
             System.out.println("5.edit contact information");
-            System.out.println("6.exit");
+            System.out.println("6.delete contact information");
+            System.out.println("7.exit");
             System.out.print("enter option:");
             String option = scanner.nextLine().trim();
-            switch (option) {
+            switch (option)
+            {
                 case "1":
                     addressBook.createNewContact();
                     break;
@@ -179,6 +248,9 @@ public class AddressBook {
                     addressBook.editContactInfo();
                     break;
                 case "6":
+                    addressBook.deleteContact();
+                    break;
+                case "7":
                     System.out.println("exiting....");
                     System.exit(0);
                 default:
@@ -190,6 +262,5 @@ public class AddressBook {
 
     }
 }
-
 
 
